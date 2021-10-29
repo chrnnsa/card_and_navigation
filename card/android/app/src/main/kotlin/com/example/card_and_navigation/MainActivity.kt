e269bd455c3401b226f35d7626c02d1a747e1000
chrnnsa <chairun089@gmail.com>
2021-10-29 13:16:24 +0700

    card and navigation

---
+++ card/android/app/src/main/kotlin/com/example/card_and_navigation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.card_and_navigation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ navigation/lib/main.dart
@@ -0,0 +1,59 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new HalamanPertama(),
+    routes: <String, WidgetBuilder>{
+      'HalamanPertama' : (BuildContext context) => new HalamanPertama(),
+      'Halaman Kedua' : (BuildContext context) => new HalamanKedua(),
+    },
+  ));
+}
+
+class HalamanPertama extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Portal Menu Akses Route'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: new IconButton(
+          icon: new Icon(
+            Icons.headset,
+            size: 50.0,
+          ),
+          onPressed: (){
+            Navigator.pushNamed(context,'/HalamanKedua');
+          },
+          ),
+      ),
+    );
+  }
+}
+
+class HalamanKedua extends StatelessWidget{
+  @override 
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("SPEAKER"),
+      ),
+      body: new Center(
+        child: new IconButton(
+          icon: new Icon(
+            Icons.speaker,
+            size: 50.0,
+            color: Colors.red, 
+          ),
+          onPressed: (){
+            Navigator.pushNamed(context, '?HalamanPertama');
+          },
+          ),
+      ),
+    );
+  }
+}
